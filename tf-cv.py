2faa9539ffe37e193f2a03c2e726a2ea523c4d25
Diazonic Labs <61781809+diazoniclabs@users.noreply.github.com>
2022-06-29 20:08:31 +0530

    Update tf-cv.py

--- tf-cv.py
+++ tf-cv.py
@@ -2,10 +2,11 @@ import numpy as np
 import cv2
 
 net = cv2.dnn.readNetFromTensorflow('final_model.pb')
+#net = cv2.dnn.readNet('tf_final.xml','tf_final.bin')
 
-label = ['Prince','Ameer']
+label = ['Category 1','Category 2']
 
-#net.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU) 
 
 vid_cap = cv2.VideoCapture(0)
@@ -17,12 +18,7 @@ while True:
     ret, frame = vid_cap.read()
     inWidth = 224
     inHeight = 224
-    blob = cv2.dnn.blobFromImage(frame,
-	                                scalefactor=1.0 / 255,
-	                                size=(inWidth, inHeight),
-	                                mean=(0, 0, 0),
-	                                swapRB=False,
-	                                crop=False)
+    blob = cv2.dnn.blobFromImage(frame, scalefactor=1.0 / 255, size=(inWidth, inHeight),mean=(0, 0, 0))
     net.setInput(blob)
     out = net.forward()
     out = out.flatten()
@@ -31,6 +27,7 @@ while True:
     op = f'{label[classId]} - {confidence}%'
     print(op)
     
+    # For FPS and Inference Time
     cv2.putText(frame, op, (30,50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
     t, _ = net.getPerfProfile()
     l = 'Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency())
